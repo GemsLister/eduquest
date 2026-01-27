f94b2434acb36a8dc736c78c02a216e48168daf5
GemsLister <jameslester.l.lopez@gmail.com>
2026-01-27 19:22:38 +0800

    test: comment all lines for testing

---
+++ apps/web/src/components/Layout.jsx
@@ -0,0 +1,17 @@
+// import { Sidebar } from "./Sidebar.jsx";
+// import { Header } from "./Header.jsx";
+// import { Outlet } from "react-router-dom";
+
+// export const Layout = ({ navs }) => {
+//   return (
+//     <div className="flex h-screen">
+//       <Sidebar navs={navs} />
+//       <div className="flex flex-col flex-1 h-screen bg-authentic-white">
+//         <Header />
+//         <main>
+//           <Outlet />
+//         </main>
+//       </div>
+//     </div>
+//   );
+// };
