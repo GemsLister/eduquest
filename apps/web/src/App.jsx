682e52e32362554b41cd64baa8b30416563e3f94
James <jameslopez@email.com>
2026-02-10 23:37:21 +0800

    style: added popup notification

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
+      <ToastContainer position="top-right" autoClose={3000} theme="colored" />
       <Outlet />
     </>
   );
