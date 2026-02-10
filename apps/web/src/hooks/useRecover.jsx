79ae57e513837b006db815f37b1c2677486ff390
James <jameslopez@email.com>
2026-02-10 23:49:20 +0800

    feat: added popup notification

--- apps/web/src/hooks/useRecover.jsx
+++ apps/web/src/hooks/useRecover.jsx
@@ -1,19 +1,25 @@
 import { supabase } from "../supabaseClient.js";
+import { toast } from "react-toastify";
 
 export const useRecover = () => {
   const handleRecover = async (userData) => {
     try {
-      const { data, error } = await supabase.auth.resetPasswordForEmail(
-        userData.email,
-        {
-          redirectTo: import.meta.env.VITE_CHANGE_PASSWORD_URL,
-        },
-      );
-      if (error) alert("Email not found");
-      else alert("Open gmail");
-      console.log(data.message);
+      if (
+        userData.email.endsWith(
+          import.meta.env.VITE_INSTRUCTOR_ACCOUNT_EXTENSION,
+        )
+      ) {
+        const { data, error } = await supabase.auth.resetPasswordForEmail(
+          userData.email,
+          {
+            redirectTo: import.meta.env.VITE_CHANGE_PASSWORD_URL,
+          },
+        );
+        toast.success("Reset password link sent to your account");
+        console.log(data.message);
+      } else toast.error("Invalid Email");
     } catch (error) {
-      console.error(error);
+      toast.error("Email not found");
     }
   };
   return { handleRecover };
