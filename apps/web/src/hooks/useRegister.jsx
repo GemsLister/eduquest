d7a6d5efc8ba239d11a842aa43980056f1a46fa9
James <jameslopez@email.com>
2026-02-10 23:49:42 +0800

    feat: added popup notification

--- apps/web/src/hooks/useRegister.jsx
+++ apps/web/src/hooks/useRegister.jsx
@@ -1,21 +1,31 @@
 import { supabase } from "../supabaseClient.js";
+import { toast } from "react-toastify";
 export const useRegister = () => {
   const handleRegister = async (userData) => {
     try {
-      const { data, error } = await supabase.auth.signUp({
-        email: userData.email.trim(),
-        password: userData.password,
-        options: {
-          data: {
-            username: userData.username.trim(),
+      if (
+        !userData.email.endsWith(
+          import.meta.env.VITE_INSTRUCTOR_ACCOUNT_EXTENSION,
+        )
+      )
+        toast.error("Institutional Account of Instructors Only!");
+      else {
+        const { data, error } = await supabase.auth.signUp({
+          email: userData.email.trim(),
+          password: userData.password,
+          options: {
+            data: {
+              username: userData.username.trim(),
+            },
+            emailRedirectTo: import.meta.env.VITE_LOGIN_URL,
           },
-          emailRedirectTo: import.meta.env.VITE_LOGIN_URL,
-        },
-      });
-      if (error) throw error;
-      console.log(data.user);
+        });
+        toast.success("Success, confirmation link sent in your account");
+        return { success: true, data };
+      }
     } catch (error) {
       console.log(error.message);
+      return { success: false, message: error.message };
     }
   };
   return { handleRegister };
