8d67cd16ebe40c0b4bea8473dd59e478031fe784
James <jameslopez@email.com>
2026-02-10 23:38:30 +0800

    feat: import react-toastify for creating popup notifications

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes.jsx";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
