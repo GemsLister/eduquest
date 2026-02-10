8c3be0d6639cad85e228c1495dc4840f4bc488c8
James <jameslopez@email.com>
2026-02-10 23:57:53 +0800

    feat: install the react-toastify for popup notifications

---
+++ apps/web/src/components/ErrorMessage.jsx
@@ -0,0 +1,9 @@
+// import { useError } from "../hooks/useError";
+// export const ErrorMessage = ({ errorMessage }) => {
+  
+//     return (
+//     <div className="text-red-500 text-sm font-bold">
+//       {errorMessage}
+//     </div>
+//   );
+// };
---
+++ apps/web/src/hooks/useError.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+// export const useError = () => {
+//   const [errorMessage, setErrorMessage] = useState("");
+//   const handleError = (userData) => {
+//     try {
+//       if (!userData.email || !userData.password) setErrorMessage("Needed");
+//     } catch (error) {
+//       console.error(error.message);
+//     }
+//   };
+//   return { handleError, errorMessage };
+// };
