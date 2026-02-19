b521b25e09bce9dbb5f3d2ba514b1e085da173a7
James <jameslopez@email.com>
2026-02-19 14:01:45 +0800

    deleted

--- apps/web/src/components/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-// import { useError } from "../hooks/useError";
-// export const ErrorMessage = ({ errorMessage }) => {
-  
-//     return (
-//     <div className="text-red-500 text-sm font-bold">
-//       {errorMessage}
-//     </div>
-//   );
-// };
