ec7206eb2c5160a9237318657c6340366ab56f3b
James <jameslopez@email.com>
2026-02-19 14:00:40 +0800

    sidebar icons

---
+++ apps/web/src/assets/svg/sidebar/sidebarIcons.js
@@ -0,0 +1,7 @@
+export * from "./DashboardIcon.jsx";
+export * from "./DockIcons.jsx";
+export * from "./SearchIcon.jsx";
+export * from "./ProfileIcon.jsx";
+export * from "./SectionIcon.jsx";
+export * from "./LogoutIcon.jsx";
+
