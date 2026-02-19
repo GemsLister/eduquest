b2201b84d699fa5d200931f916c3012d8709361d
James <jameslopez@email.com>
2026-02-19 14:01:56 +0800

    update variables

--- apps/web/src/supabaseClient.js
+++ apps/web/src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabasePublishableDefaultKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+export const supabase = createClient(supabaseUrl, supabasePublishableDefaultKey);
