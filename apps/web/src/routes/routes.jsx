69c2c3e75d0b103169d2cdc86013c5fd6003197b
James <jameslopez@email.com>
2026-02-21 19:55:56 +0800

    ref: comment all student routes

--- apps/web/src/routes/routes.jsx
+++ apps/web/src/routes/routes.jsx
@@ -16,12 +16,12 @@ import { SectionDetail } from "../pages/instructors/SectionDetail.jsx";
 import { QuizResults } from "../pages/instructors/quizzes/QuizResults.jsx";
 import { QuizResultDetail } from "../pages/instructors/quizzes/QuizResultDetail.jsx";
 // Student Routes
-import { StudentLayout } from "../pages/students/StudentLayout.jsx";
-import { StudentDashboard } from "../pages/students/StudentDashboard.jsx";
-import { StudentQuiz } from "../pages/students/StudentQuiz.jsx";
-import { StudentScores } from "../pages/students/StudentScores.jsx";
-import { StudentSummaries } from "../pages/students/StudentSummaries.jsx";
-import { StudentProfile } from "../pages/students/StudentProfile.jsx";
+// import { StudentLayout } from "../pages/students/StudentLayout.jsx";
+// import { StudentDashboard } from "../pages/students/StudentDashboard.jsx";
+// import { StudentQuiz } from "../pages/students/StudentQuiz.jsx";
+// import { StudentScores } from "../pages/students/StudentScores.jsx";
+// import { StudentSummaries } from "../pages/students/StudentSummaries.jsx";
+// import { StudentProfile } from "../pages/students/StudentProfile.jsx";
 // Public Routes
 import { PublicQuizPage } from "../pages/PublicQuizPage.jsx";
 
@@ -97,36 +97,36 @@ export const router = createBrowserRouter(
           ],
         },
         // For student routing
-        {
-          path: "student-dashboard",
-          element: (
-            <ProtectedRoute>
-              <StudentLayout />
-            </ProtectedRoute>
-          ),
-          children: [
-            {
-              index: true,
-              element: <StudentDashboard />,
-            },
-            {
-              path: "student-quiz",
-              element: <StudentQuiz />,
-            },
-            {
-              path: "student-scores",
-              element: <StudentScores />,
-            },
-            {
-              path: "student-summaries",
-              element: <StudentSummaries />,
-            },
-            {
-              path: "student-profile",
-              element: <StudentProfile />,
-            },
-          ],
-        },
+        // {
+        //   path: "student-dashboard",
+        //   element: (
+        //     <ProtectedRoute>
+        //       <StudentLayout />
+        //     </ProtectedRoute>
+        //   ),
+        //   children: [
+        //     {
+        //       index: true,
+        //       element: <StudentDashboard />,
+        //     },
+        //     {
+        //       path: "student-quiz",
+        //       element: <StudentQuiz />,
+        //     },
+        //     {
+        //       path: "student-scores",
+        //       element: <StudentScores />,
+        //     },
+        //     {
+        //       path: "student-summaries",
+        //       element: <StudentSummaries />,
+        //     },
+        //     {
+        //       path: "student-profile",
+        //       element: <StudentProfile />,
+        //     },
+        //   ],
+        // },
       ],
     },
   ],
