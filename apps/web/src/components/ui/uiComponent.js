320674da83f3186f43025bd7572156e906be3e7d
James <jameslopez@email.com>
2026-02-21 17:46:42 +0800

    feat: add export of components

---
+++ apps/web/src/components/ui/uiComponent.js
@@ -0,0 +1,3 @@
+export { PrimaryButton } from "./buttons/PrimaryButton.jsx";
+export { CreateClassPopup } from "./forms/CreateClassPopup.jsx";
+export { SearchInput } from "./inputs/SearchInput.jsx";
