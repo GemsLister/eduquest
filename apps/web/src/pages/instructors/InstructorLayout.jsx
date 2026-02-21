2363c2bd59f912e5931a549e9a90540ff593c1a6
James <jameslopez@email.com>
2026-02-21 19:56:38 +0800

    ref: exported  the sidebar icons holder

--- apps/web/src/pages/instructors/InstructorLayout.jsx
+++ apps/web/src/pages/instructors/InstructorLayout.jsx
@@ -1,34 +1,35 @@
 import { Sidebar } from "../../components/Sidebar.jsx";
 import { Header } from "../../components/Header.jsx";
-import { DashboardIcon } from "../../assets/svg/DashboardIcon.jsx";
-import { QuizIcon } from "../../assets/svg/QuizIcon.jsx";
-import { QuestionIcon } from "../../assets/svg/sidebar/QuestionIcon.jsx";
-import { ProfileIcon } from "../../assets/svg/ProfileIcon.jsx";
-import { LogoutIcon } from "../../assets/svg/LogoutIcon.jsx";
+// import { DashboardIcon } from "../../assets/svg/DashboardIcon.jsx";
+// import { QuizIcon } from "../../assets/svg/QuizIcon.jsx";
+// import { QuestionIcon } from "../../assets/svg/sidebar/QuestionIcon.jsx";
+// import { ProfileIcon } from "../../assets/svg/ProfileIcon.jsx";
+// import { LogoutIcon } from "../../assets/svg/LogoutIcon.jsx";
 import { Outlet } from "react-router-dom";
 import InstructorImage from "../../assets/instructor-profile.png";
+import * as SidebarIcon from "../../assets/svg/sidebar/sidebarIcons.js"
 
 export const InstructorLayout = () => {
   const navs = [
     {
       name: "Home",
       path: "/instructor-dashboard",
-      icon: <DashboardIcon />,
+      icon: <SidebarIcon.DashboardIcon />,
     },
     {
       name: "Questions",
       path: "/instructor-dashboard/instructor-questions",
-      icon: <QuestionIcon />,
+      icon: <SidebarIcon.QuestionIcon />,
     },
     {
       name: "Profile",
       path: "/instructor-dashboard/instructor-profile",
-      icon: <ProfileIcon />,
+      icon: <SidebarIcon.ProfileIcon />,
     },
     {
       name: "Logout",
       path: "/",
-      icon: <LogoutIcon />,
+      icon: <SidebarIcon.LogoutIcon />,
     },
   ];
 
