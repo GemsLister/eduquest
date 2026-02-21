17d5e60fb38b38d27d602500876377351a225984
James <jameslopez@email.com>
2026-02-21 19:34:36 +0800

    feat: added auth hooks

---
+++ apps/web/src/hooks/authHook/authHooks.js
@@ -0,0 +1,6 @@
+export { useGoogleLogin } from "./useGoogleLogin.jsx";
+export { useLogin } from "./useLogin.jsx";
+export { useRecover } from "./useRecover.jsx";
+export { useRegister } from "./useRegister.jsx";
+export { useChangePassword } from "./useChangePassword.jsx";
+export { useLogout } from "./useLogout.jsx";
