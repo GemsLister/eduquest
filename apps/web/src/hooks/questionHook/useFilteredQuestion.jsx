626ed10be86a981afe54d3ff71f49676fc03e719
James <jameslopez@email.com>
2026-02-21 20:44:24 +0800

    ref: added questions prop and add more return

--- apps/web/src/hooks/questionHook/useFilteredQuestion.jsx
+++ apps/web/src/hooks/questionHook/useFilteredQuestion.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
-export const useFilteredQuestion = () => {
+export const useFilteredQuestion = (questions = []) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filterType, setFilterType] = useState("all");
 
-  const filteredQuestions = questions.filter((q) => {
-    const matchesSearch = q.text
+  const filteredQuestions = questions.filter((question) => {
+    const matchesSearch = question.text
       .toLowerCase()
       .includes(searchTerm.toLowerCase());
     const matchesType = filterType === "all" || q.type === filterType;
     return matchesSearch && matchesType;
   });
 
-  return { filteredQuestions, setSearchTerm, setFilterType };
+  return { filteredQuestions, searchTerm, setSearchTerm, setFilterType };
 };
