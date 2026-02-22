8aea16342aa307f952c42d4f4100eea22fb7ef71
James <jameslopez@email.com>
2026-02-22 16:10:53 +0800

    feat: created instructorPageIndex for importing instructor pages routes with less lines

---
+++ apps/web/src/pages/instructors/instructorPageIndex.js
@@ -0,0 +1,7 @@
+export { InstructorDashboard } from "./InstructorDashboard.jsx";
+export { InstructorQuiz } from "./InstructorQuiz.jsx";
+export { InstructorQuestions } from "./InstructorQuestions.jsx";
+export { InstructorProfile } from "./InstructorProfile.jsx";
+export { SectionDetail } from "./SectionDetail.jsx";
+export { QuizResults } from "./QuizResults.jsx";
+export { QuizResultDetail } from "./QuizResultDetail.jsx";
