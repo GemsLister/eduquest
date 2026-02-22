1e4ff7db6b2630d5ae2a22659b6d6dbbc28f1404
James <jameslopez@email.com>
2026-02-22 16:10:22 +0800

    feat: created authIndex for importing auth routes with less lines

---
+++ apps/web/src/pages/auth/authIndex.js
@@ -0,0 +1,4 @@
+export { Login } from "./Login.jsx";
+export { Register } from "./Register.jsx";
+export { RecoverPassword } from "./RecoverPassword.jsx";
+export { ChangePassword } from "./ChangePassword.jsx";
