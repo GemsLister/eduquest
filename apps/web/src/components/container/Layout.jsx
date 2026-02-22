2b2f02c2427f83ba783eba7da61b9b506602d7d0
James <jameslopez@email.com>
2026-02-22 16:17:35 +0800

    fix: solved the overlap issue

--- apps/web/src/components/container/Layout.jsx
+++ apps/web/src/components/container/Layout.jsx
@@ -5,9 +5,9 @@ import InstructorImage from "../../assets/instructor-profile.png";
 
 export const Layout = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <div className="flex flex-col flex-1 h-screen bg-authentic-white">
+      <div className="flex flex-col flex-1 h-screen overflow-y-auto bg-authentic-white">
         <Header profile={InstructorImage} />
         <main>
           <Outlet />
