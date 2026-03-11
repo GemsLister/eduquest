256262c3287e192c0e35ce9e4a1cc652a0764eec
James <jameslopez@email.com>
2026-03-11 21:54:59 +0800

    feat: add useQuestionBank hook

--- apps/web/src/hooks/questionHook/questionHooks.js
+++ apps/web/src/hooks/questionHook/questionHooks.js
@@ -2,3 +2,4 @@ export { useAddSaveQuestion } from "./useAddSaveQuestion.jsx";
 export { useDeleteQuestion } from "./useDeleteQuestion.jsx";
 export { useFetchQuestion } from "./useFetchQuestions.jsx";
 export { useFilteredQuestion } from "./useFilteredQuestion.jsx";
+export { useQuestionBank } from "./useQuestionBank.jsx";
