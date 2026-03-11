ab634005550e5b95fa90b2e3348606188370a199
James <jameslopez@email.com>
2026-03-11 22:00:35 +0800

    feat: add QuestionBank page

--- apps/web/src/pages/instructors/instructorPageIndex.js
+++ apps/web/src/pages/instructors/instructorPageIndex.js
@@ -1,6 +1,7 @@
 export { InstructorDashboard } from "./InstructorDashboard.jsx";
 export { InstructorQuiz } from "./InstructorQuiz.jsx";
 export { InstructorQuestions } from "./InstructorQuestions.jsx";
+export { QuestionBank } from "./QuestionBank.jsx";
 export { InstructorProfile } from "./InstructorProfile.jsx";
 export { SectionDetail } from "./SectionDetail.jsx";
 export { QuizResults } from "./QuizResults.jsx";
