0092b1df2c1a40defaf647934cf0fe32e3f09317
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-07-27 00:12:38 +0800

    add faculty head trail in export barrel

--- apps/web/src/pages/faculty-head/facultyHeadPageIndex.js
+++ apps/web/src/pages/faculty-head/facultyHeadPageIndex.js
@@ -2,3 +2,4 @@ export { FacultyHeadDashboard } from "./FacultyHeadDashboard.jsx";
 export { FacultyHeadQuizApprovals } from "./FacultyHeadQuizApprovals.jsx";
 export { FacultyHeadApprovalDetail } from "./FacultyHeadApprovalDetail.jsx";
 export { FacultyHeadSettings } from "./FacultyHeadSettings.jsx";
+export { FacultyHeadAuditTrail } from "./FacultyHeadAuditTrail.jsx";
