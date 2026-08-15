0e8d8c5d64edace99888eea07156c7b5861275bf
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-08-15 14:49:03 +0800

    exported faculty head subject requests

--- apps/web/src/pages/faculty-head/facultyHeadPageIndex.js
+++ apps/web/src/pages/faculty-head/facultyHeadPageIndex.js
@@ -1,5 +1,6 @@
 export { FacultyHeadDashboard } from "./FacultyHeadDashboard.jsx";
 export { FacultyHeadQuizApprovals } from "./FacultyHeadQuizApprovals.jsx";
 export { FacultyHeadApprovalDetail } from "./FacultyHeadApprovalDetail.jsx";
+export { FacultyHeadSubjectRequests } from "./FacultyHeadSubjectRequests.jsx";
 export { FacultyHeadSettings } from "./FacultyHeadSettings.jsx";
 export { FacultyHeadAuditTrail } from "./FacultyHeadAuditTrail.jsx";
